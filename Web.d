6029b92bdfa9e06b3757f675e3dc7d9924ca672b
Tivatamber <tivatamber@gmail.com>
2023-05-05 08:33:17 +0800

    Can Use

--- Web.d
+++
@@ -1,1 +0,0 @@
-C:\Users\86189\CLionProjects\Web\target\release\Web.exe: C:\Users\86189\CLionProjects\Web\src\lib.rs C:\Users\86189\CLionProjects\Web\src\main.rs
--- libWeb.d
+++
@@ -1,1 +0,0 @@
-C:\Users\86189\CLionProjects\Web\target\release\libWeb.rlib: C:\Users\86189\CLionProjects\Web\src\lib.rs
